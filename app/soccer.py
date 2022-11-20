67c527aeb0a33135450b10e7ab8342e95e1fd461
Bjorn Van den Dungen <r0889357@student.thomasmore.be>
2022-11-20 01:49:37 +0100

    Update soccer.py

--- app/soccer.py
+++ app/soccer.py
@@ -1,10 +1,12 @@
+import random
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-#implementing security
+# implementing security
 origins = [
     "http://localhost",
     "http://localhost:8080",
@@ -21,7 +23,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#1ste basemodel voor player
+
+# 1ste basemodel voor player
 class Player(BaseModel):
     first_name: str | None = "Invalid"
     last_name: str | None = "Invalid"
@@ -29,7 +32,8 @@ class Player(BaseModel):
     number: int | None = "Invalid"
     birth_date: str | None = None
 
-#2de basemodel voor team
+
+# 2de basemodel voor team
 class Team(BaseModel):
     team_name: str | None = "Invalid"
     captain_team: str | None = None
@@ -43,11 +47,31 @@ team_real_madrid = {
     "played_games": 6,
     "ranking": 7
 }
+
+team_liverpool = {
+    "team_name": "Liverpool",
+    "captain_team": "Mohammed Salah",
+    "played_games": 5,
+    "ranking": 3
+}
 team_manchester_united = {
     "team_name": "Manchester United",
     "captain_team": "Paul Pogba",
-    "played_games": 5,
+    "played_games": 4,
     "ranking": 4
+}
+team_southampton = {
+    "team_name": "Southampton FC",
+    "captain_team": "James Ward-Prowse",
+    "played_games": 5,
+    "ranking": 2
+}
+
+team_chelsea = {
+    "team_name": "Chelsea",
+    "captain_team": "Lampard",
+    "played_games": 6,
+    "ranking": 1
 }
 
 first_player = {
@@ -77,7 +101,7 @@ third_player = {
 
 }
 
-team_list = [team_real_madrid, team_manchester_united]
+team_list = [team_real_madrid, team_manchester_united, team_chelsea, team_liverpool, team_southampton]
 player_list = [first_player, second_player, third_player]
 
 
@@ -86,14 +110,18 @@ async def create_player(player: Player):
     return player
 
 
-@app.get("/teams", response_model=list)
-async def get_teams():
-    return team_list
+@app.get("/teams/{ranking_position}", response_model=Team)
+async def get_teams(ranking_position: int):
+    for team in team_list:
+        if team.get("ranking") == ranking_position:
+            return team
+    return "Team niet in de ranking"
 
 
 @app.get("/player/{player_name}", response_model=Player)
 async def get_player(player_name: str):
     for player in player_list:
-        if player.get("first_name") == player_name: #kijkt naar de firstname van de speler en geeft dan bijkomende informatie erbij
+        if player.get(
+                "first_name") == player_name:  # kijkt naar de firstname van de speler en geeft dan bijkomende informatie erbij
             return player
     return "Speler niet mogelijk"
